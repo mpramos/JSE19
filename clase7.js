448170d76a12fc6421926fb3bf8557911d0c0c5a
maria <mpramos78915400@gmail.com>
2026-03-25 21:18:29 -0400

    clase 7, condicionales para el sistema de votaciones

--- clase7.js
+++ clase7.js
@@ -9,12 +9,50 @@ if (respuesta === true) {
     alert('vuelve pronto')
 }
 
-let edad = prompt('Ingrese su edad')
+let edad = prompt('Cuántos años tienes?, ingrese su edad')
 if (edad >= 18) {
-    alert('eres mayor de edad, puedes votar')
+    let res = prompt('Tiene su carnet?, responde con un si o no')
+    console.log(res)
+    let tieneCarnet = res === 'si' ? true : false
+    if (tieneCarnet) {
+        alert('Puedes votar 👍');
+    } else {
+        alert('No puedes votar ❎')
+    }
 } else {
-    alert('eres menor de edad, aún no puedes votar')
+    alert('eres menor de edad, no puedes votar')
+    
 }
 
-let nombre = 'susana'
-let puntos = 0
+
+
+
+if (true === true) {
+    console.log('se cumple')
+}else{
+    console.log('no se cumple')
+}
+if (false === false) {
+    console.log('se cumple')
+}else{
+    console.log('no se cumple')
+}
+if(true){
+    console.log('se cumple')
+}else{console.log('no se cumple')}
+if(false){
+    console.log('se cumple')
+}else{console.log('no se cumple')}
+if('mario'){
+    console.log('se cumple')
+}else{console.log('no se cumple')}
+if(''){
+    console.log('se cumple')
+}else{console.log('no se cumple')}
+if(0){
+    console.log('se cumple')
+}else{console.log('no se cumple')}
+if(-1223){
+    console.log('se cumple')
+}else{console.log('no se cumple')}
+
